79e1df8f3e003f15b1d868f07cfec474ccd0a806
mhdnabi-swift <marley.ussain45@gamil.com>
2025-05-19 18:09:18 +0530

    new

---
+++ printsumofarray2d.java
@@ -0,0 +1,19 @@
+public class printsumofarray2d {
+    
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        for (int i = 0; i < arr.length; i++) {  
+            int sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {  
+                sum += arr[i][j];
+            }
+            System.out.println("Row " + (i + 1) + " Sum = " + sum);
+        }
+    }
+}
+
+    
